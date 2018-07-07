781e834f5b5026b6f31f824e342f9270d0c316a5
user <a0981327636@gmail.com>
2018-07-07 10:11:53 +0800

    20180707_itemMenu_passVar

---
+++ src/item2.js
@@ -0,0 +1,45 @@
+
+var Item2Layer = cc.Layer.extend({
+    sprite:null,
+    ctor:function () {
+
+        this._super();
+        this.initMenu();
+
+        return true;
+    },
+
+    initMenu: function () {
+        var back = new cc.MenuItemImage(
+            res.back_normal_png,
+            res.back_selected_png,
+            res.back_disselect_png,
+            this.back, this
+        );
+
+        var menu = new cc.Menu(back);
+        this.addChild(menu);
+    },
+
+    back: function () {
+        cc.director.popScene();
+    },
+});
+
+var Item2Scene = cc.Scene.extend({
+    mydata: null,
+    ctor: function(data){
+      this._super();
+      this.mydata = data;
+      cc.log("item2Scene:ctor():"+data);
+      // var layer = new Item2Layer(); //讓life cycle做
+      // this.addChild(layer);
+    },
+    onEnter:function () {
+        //this._super();
+        cc.log("item2Scene:onEnter()"+this.mydata); //ctor才可以傳參數
+        var layer = new Item2Layer();
+        this.addChild(layer);
+    }
+});
+
